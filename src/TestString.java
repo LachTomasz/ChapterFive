1dc2f2bb265b0862ee3a6693be86bd7b4c81bedc
LachTomasz <lach.tomasz@gmail.com>
2022-02-15 23:44:38 +0100

    Initial commit

---
+++ src/TestString.java
@@ -0,0 +1,20 @@
+public class TestString {
+    public static void main(String[] args) {
+        System.out.println("Zadania z programowania / programming tasks.");
+        System.out.println("Zadania z programowania / programming tasks.".charAt(0));
+        System.out.println("Zadania z programowania / programming tasks.".length());
+        System.out.println("Zadania z programowania / programming tasks.".charAt(24));
+        System.out.println("Zadania z programowania / programming tasks.".toUpperCase());
+        System.out.println("Zadania z programowania / programming tasks.".toLowerCase());
+        System.out.println("Zadania z programowania / programming tasks.".indexOf('z'));
+        System.out.println("Zadania z programowania / programming tasks.".indexOf("prog"));
+        System.out.println("Zadania z programowania / programming tasks.".replace('.','?'));
+        System.out.println("Zadania z programowania / programming tasks.".replace("adania","dania"));
+        System.out.println("Zadania z programowania / programming tasks.".replaceAll("ania","anka"));
+        System.out.println("Zadania z programowania / programming tasks.".replaceFirst("ania","anka"));
+        System.out.println("Zadania z programowania / programming tasks.".substring(10));
+        System.out.println("Zadania z programowania / programming tasks.".substring(10,17));
+        System.out.println("Zadania z programowania / programming tasks.".concat("\b z podpowiedziami"));
+        System.out.println("Zadania z programowania / programming tasks."+"\b"+" z odpowiedziami");
+    }
+}
