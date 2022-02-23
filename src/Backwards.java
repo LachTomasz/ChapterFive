e63a84a4e1e44e521d3394ab1c5479eeb929db0c
LachTomasz <lach.tomasz@gmail.com>
2022-02-23 22:55:57 +0100

    Initial commit, exercise 4.5

---
+++ src/Backwards.java
@@ -0,0 +1,9 @@
+public class Backwards {
+    public static void main(String[] args) {
+        String text = "programming";
+        System.out.println();
+        for(int i = text.length() - 1; i>=0; i--){
+            System.out.print(text.charAt(i));
+        }
+    }
+}
